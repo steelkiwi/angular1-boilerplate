557b9d8c511aa0ff282b12954cd6070ee0f9f6ed
uzumaxy <uzumaxy@gmail.com>
2016-08-25 20:39:57 +0300

    Defined constants.

---
+++ src/constants/features.js
@@ -0,0 +1,23 @@
+(() => {
+    'use strict';
+
+    angular
+        .module('app')
+        .constant('projectFeaturesList', [
+            'Gulp',
+            'Angular 1.5 (component-oriented)',
+            'Angular UI Router',
+            'Bootstrap 3 (Less)',
+            'Bower',
+            'Script injection',
+            'BrowserSync',
+            'ES6 with Babel',
+            'ESLint',
+            'Less',
+            'Autoprefixer',
+            'HTML/JS/CSS minification, uglification, concatenation',
+            'Sourcemaps',
+            'Karma, Jasmine and PhantomJS for testing',
+            'Django CSRF protection support'
+        ]);
+})();
