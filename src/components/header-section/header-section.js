d2c09c6d7042b9c7c603ab58841031d5106cbedc
uzumaxy <uzumaxy@gmail.com>
2016-08-25 20:42:33 +0300

    Header-section component.

---
+++ src/components/header-section/header-section.js
@@ -0,0 +1,9 @@
+(() => {
+    'use strict';
+
+    angular
+        .module('app')
+        .component('headerSection', {
+            templateUrl: 'components/header-section/header-section.html'
+        });
+})();
